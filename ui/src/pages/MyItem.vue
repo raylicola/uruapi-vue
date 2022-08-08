fa660a43e2289e5c542d8f4c9610d2ccb806d536
raylicola <rope.champ.109@gmail.com>
2022-08-08 15:34:20 +0900

    出品一覧画面

---
+++ ui/src/pages/MyItem.vue
@@ -0,0 +1,66 @@
+<template>
+<small-space />
+<navigation-drawer />
+<div >
+  <v-row>
+    <item-card
+      v-for="item in items"
+      :key="item.ID"
+      :title="item.Title"
+      :price="item.Price"
+      :img="item.Img"
+      @click="toDetail(item.ID)"
+    />
+  </v-row>
+</div>
+</template>
+
+<script>
+import { ItemCard, SmallSpace, NavigationDrawer } from '@/components'
+import axios from 'axios'
+import { onMounted, watch, ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+
+export default {
+  name: 'MyItem',
+  components: {
+    'item-card': ItemCard,
+    'small-space': SmallSpace,
+    'navigation-drawer': NavigationDrawer,
+  },
+  setup(){
+    const router = useRouter()
+    const store = useStore()
+
+
+    const items = ref([])
+    const user_id = computed(() => store.state.user_id)
+
+    const getMyItem = async () => {
+      const url = '/user/' + user_id.value + '/item'
+      const {data} = await axios.get(url)
+      items.value = data.items
+      console.log(data)
+    }
+
+    const toDetail = (item_id) => {
+      router.push('/item/' + item_id)
+    }
+
+    onMounted(async () => {
+      getMyItem()
+    })
+
+    watch(
+      items, () => items.value,
+    )
+
+    return {
+      items,
+      toDetail,
+    }
+  }
+}
+
+</script>
