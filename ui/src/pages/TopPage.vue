d5471e0ef961026b77065ce7625a3e625c0e639c
raylicola <rope.champ.109@gmail.com>
2022-08-08 13:22:15 +0900

    キーワード検索機能を実装

--- ui/src/pages/TopPage.vue
+++ ui/src/pages/TopPage.vue
@@ -1,4 +1,11 @@
 <template>
+<v-form @submit.prevent>
+  <base-text-field
+  @keyup.enter="searchWish"
+  label="キーワードで検索"
+  v-model="keyword"
+/>
+</v-form>
 <small-space />
 <div >
   <v-row>
@@ -14,7 +21,7 @@
 </template>
 
 <script>
-import { WishCard, SmallSpace } from '@/components'
+import { WishCard, SmallSpace, BaseTextField } from '@/components'
 import axios from 'axios'
 import { onMounted, watch, ref } from 'vue';
 import { useRouter } from 'vue-router';
@@ -23,11 +30,13 @@ export default {
   name: 'TopPage',
   components: {
     'base-card': WishCard,
-    'small-space': SmallSpace
+    'small-space': SmallSpace,
+    'base-text-field': BaseTextField,
   },
   setup(){
 
     const wishes = ref([])
+    const keyword = ref('')
 
     const router = useRouter()
 
@@ -35,11 +44,16 @@ export default {
       const url = 'wish'
       const {data} = await axios.get(url)
       wishes.value = data.wishes
-      console.log(wishes)
     }
 
     const toDetail = (wish_id) => {
       router.push('/wish/' + wish_id)
+    }
+
+    const searchWish = async () => {
+      const url = '/wish/search?keyword=' + keyword.value
+      const {data} = await axios.get(url)
+      wishes.value = data.wishes
     }
 
     onMounted(async () => {
@@ -52,7 +66,9 @@ export default {
 
     return {
       wishes,
+      keyword,
       toDetail,
+      searchWish
     }
   }
 }
