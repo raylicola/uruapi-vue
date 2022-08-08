c49f4693c0bb796993a7a348c8f2cd0961178315
raylicola <rope.champ.109@gmail.com>
2022-08-08 15:31:33 +0900

    マイページトップを作成

--- ui/src/pages/MyPage.vue
+++ ui/src/pages/MyPage.vue
@@ -1,64 +1,28 @@
 <template>
-<small-space />
-<div >
-  <v-row>
-    <item-card
-      v-for="item in items"
-      :key="item.ID"
-      :title="item.Title"
-      :price="item.Price"
-      :img="item.Img"
-      @click="toDetail(item.ID)"
-    />
-  </v-row>
-</div>
+  <small-space />
+  <navigation-drawer />
+  <div>{{username}}</div>
+
 </template>
 
 <script>
-import { ItemCard, SmallSpace } from '@/components'
-import axios from 'axios'
-import { onMounted, watch, ref, computed } from 'vue';
-import { useRouter } from 'vue-router';
-import { useStore } from 'vuex';
+import { SmallSpace, NavigationDrawer } from '@/components'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
   name: 'MyPage',
   components: {
-    'item-card': ItemCard,
-    'small-space': SmallSpace
+    'small-space': SmallSpace,
+    'navigation-drawer': NavigationDrawer,
   },
-  setup(){
-    const router = useRouter()
+  setup() {
     const store = useStore()
-
-
-    const items = ref([])
-    const user_id = computed(() => store.state.user_id)
-
-    const getMyPost = async () => {
-      const url = '/mypost/' + user_id.value
-      const {data} = await axios.get(url)
-      items.value = data.items
-      console.log(data)
-    }
-
-    const toDetail = (item_id) => {
-      router.push('/item/' + item_id)
-    }
-
-    onMounted(async () => {
-      getMyPost()
-    })
-
-    watch(
-      items, () => items.value,
-    )
+    const username = computed(() => store.state.user_name)
 
     return {
-      items,
-      toDetail,
+      username
     }
   }
 }
-
 </script>
