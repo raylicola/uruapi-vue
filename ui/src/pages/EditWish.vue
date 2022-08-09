1396d3882e89aeaa8a80bdac1fcd234e22216763
raylicola <rope.champ.109@gmail.com>
2022-08-09 14:03:34 +0900

    遷移先を修正

--- ui/src/pages/EditWish.vue
+++ ui/src/pages/EditWish.vue
@@ -61,7 +61,7 @@ export default {
         params.append('detail', detail.value)
         params.append('user_id', user_id.value)
         await axios.put(url, params)
-        router.push('/mypage')
+        router.push('/mypage/wish')
       } catch (e) {
         console.log(e)
       }
